eaa981c725b78f9cb4d1ba2e2dbad9cc7aea0417
James D Vieyra <james@vieyra.co.za>
2022-06-22 13:47:10 +0200

    Initial Commit

---
+++ IACD_EducationApp_StudentPortal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IACD_EducationApp_StudentPortal
+//
+//  Created by IACD-011 on 2022/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IACD_EducationApp_StudentPortal/IACD_EducationApp_StudentPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IACD_EducationApp_StudentPortalApp.swift
+//  IACD_EducationApp_StudentPortal
+//
+//  Created by IACD-011 on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct IACD_EducationApp_StudentPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
