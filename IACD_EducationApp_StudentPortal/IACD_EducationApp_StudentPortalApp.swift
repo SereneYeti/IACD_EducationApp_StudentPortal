71f460fd8850cc993a6c573f714eb159a674718e
James D Vieyra <james@vieyra.co.za>
2022-07-05 17:14:55 +0200

    Firebase Firestore & Auth Setup

--- IACD_EducationApp_StudentPortal/IACD_EducationApp_StudentPortalApp.swift
+++ IACD_EducationApp_StudentPortal/IACD_EducationApp_StudentPortalApp.swift
@@ -4,11 +4,30 @@
 //
 //  Created by IACD-011 on 2022/06/22.
 //
-
 import SwiftUI
+import Firebase
+
+/*
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}*/
 
 @main
 struct IACD_EducationApp_StudentPortalApp: App {
+    
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    // register app delegate for Firebase setup
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Pods/Target Support Files/Pods-IACD_EducationApp_StudentPortal/Pods-IACD_EducationApp_StudentPortal-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IACD_EducationApp_StudentPortal : NSObject
+@end
+@implementation PodsDummy_Pods_IACD_EducationApp_StudentPortal
+@end
