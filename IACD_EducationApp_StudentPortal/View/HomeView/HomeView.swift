19161c6bbf74f9550b2f676d1585ef11f5279e25
James D Vieyra <james@vieyra.co.za>
2022-07-18 16:53:09 +0200

    Adding Upcoming tasks view onTap & updating mapView

--- IACD_EducationApp_StudentPortal/View/HomeView/HomeView.swift
+++ IACD_EducationApp_StudentPortal/View/HomeView/HomeView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject var viewModel: AppViewModel
-    @State var showWelcomeScreen: Bool = false
+    
+    @State private var showWelcomeScreen: Bool = false
+    @State private var showMapView: Bool = false
+    @State private var showCalendarView: Bool = false
     
     var body: some View {
         NavigationView {
@@ -44,16 +47,23 @@ struct HomeView: View {
                     
                     UpcomingProjectsView()
                         .frame(height:200)
+                        .onTapGesture {
+                            showCalendarView = true
+                        }
                     
                     Text("Map View: ")
                     
                     MapView()
                         .frame(height:250)
+                        .onTapGesture {
+                            showMapView = true
+                        }
                 }
             }
             .navigationBarHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-            .sheet(isPresented: $showWelcomeScreen, content: {
-            CalendarView() })
+            .sheet(isPresented: $showWelcomeScreen, content: {CalendarView() })
+            .sheet(isPresented: $showMapView, content: {  MapView() })
+            .sheet(isPresented: $showCalendarView, content: { CalendarView() })
         }.onAppear{
             print("View Model Sign In: \(viewModel.firstTimeSignIn)")
             showWelcomeScreen = viewModel.firstTimeSignIn
