8798766d7c4c0fefc1ec76e98fd1b687d3fe0d1c
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-07-25 22:01:02 +0200

    :notebook: Student Card View Done
    Student card view and all inner components are done

--- IACD_EducationApp_StudentPortal/View/Profile/StudentCardView.swift
+++ IACD_EducationApp_StudentPortal/View/Profile/StudentCardView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct StudentCardView: View {
     @State var showCard: Bool = false
-    var testStu = testData[0]
+    @State var testStu = testData[0]
     var body: some View {
         VStack {
             VStack{
                 HStack{
                     Button {
-                        
+                        //MARK: ADD student Card and generate QR code
                     } label: {
                         Image(systemName: "plus.app")
                             .foregroundColor(.primary)
@@ -32,33 +32,17 @@ struct StudentCardView: View {
                     .padding(.trailing)
                     
                 }
-                
+                Spacer()
+                    .frame(height:  40)
                 HStack {
+                    
                     VStack(alignment: .leading) {
                         Text(testStu.firstName)
-                            .font(.system(size: 45, weight: .bold))
+                            .font(.system(size: 80, weight: .bold))
                         Text(testStu.lastName)
-                            .font(.system(size: 45, weight: .bold))
+                            .font(.system(size: 55, weight: .bold))
                     }
                     .hLeading()
-                }
-                .padding()
-                
-                HStack{
-                    Text("Student Number:")
-                        .font(.system(size: 15))
-                    Spacer()
-                    Text(String(testStu.studentNum))
-                        .font(.system(size: 20,weight: .bold))
-                }
-                .padding()
-                HStack{
-                    Text("Qualification : ")
-                    Spacer()
-                    VStack {
-                        Text("Game Design")
-                            .font(.system(size: 20,weight: .bold))
-                    }
                 }
                 .padding()
             }
@@ -66,27 +50,19 @@ struct StudentCardView: View {
             .animation(.spring(response: 0.5, dampingFraction: 0.9, blendDuration: 0), value: showCard)
             Spacer()
             Rectangle()
-//                .offset(y: showCard ? -100 : 0)
-                .frame(width: screen.width - 20, height: showCard ? screen.height + 250 : screen.height - 450 )
+                //                .offset(y: showCard ? -100 : 0)
+                .frame(width: screen.width - 20, height: showCard ? screen.height + 150 : screen.height - 450)
                 .cornerRadius(showCard ? 30 : 0)
                 .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
                 .overlay {
                     VStack {
-                        HStack(alignment: .top) {
-                         
-                            Image("qrCode")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .foregroundColor(.white)
-                                .padding()
-                        }
+                            //MARK: Inner card view
+                        InnerStudentCardView(showCard: $showCard, testStu: $testStu)
                         Spacer()
-                            .frame(height: 30)
-                        
+                            .frame(height: 10)
                         HStack {
                             VStack(alignment: .leading){
-                                Text("Degree")
-                                Text(String(testStu.studentNum))
+                                Text(showCard ? "Hide":"Show Student Card")
                             }
                             .foregroundColor(.white)
                             Spacer()
@@ -98,17 +74,12 @@ struct StudentCardView: View {
                                     .font(.system(size: 35,weight: .light))
                                     .rotationEffect(Angle(degrees: showCard ? 360 : 180))
                             }
-                            
                         }
-                        .offset(y: showCard ? -50 : -20)
+                        .offset(y: showCard ? 5 : -20)
                         .padding()
                     }
-                    
                 }
                 .animation(.spring(response: 0.6, dampingFraction: 0.9, blendDuration: 0.5), value: showCard)
-          
-            
-            
         }
     }
 }
@@ -116,5 +87,60 @@ struct StudentCardView: View {
 struct StudentCardView_Previews: PreviewProvider {
     static var previews: some View {
         StudentCardView()
+    }
+}
+
+struct InnerStudentCardView: View {
+    @Binding var showCard: Bool
+    @Binding var testStu : Student
+    var body: some View {
+        VStack(spacing: 10){
+            HStack(spacing: 20){
+                Text(testStu.firstName)
+                    .font(.system(size: 36, weight: .bold))
+                Text(testStu.lastName)
+                    .font(.system(size: 36, weight: .bold))
+            }
+            .padding()
+            Spacer()
+                .frame(height: showCard ? 10 : 0)
+            HStack{
+                Text("Course ")
+                Spacer()
+                Text("Game Design")
+            }
+            .padding()
+            HStack{
+                Text("Student Number")
+                    .fontWeight(.light)
+                Spacer()
+                Text(String(testStu.studentNum))
+            }
+            .padding()
+            HStack {
+                Text("ID Number")
+                Spacer()
+                Text(String(23456789098765))
+            }
+            .padding()
+            HStack{
+                Text("Year of issue ")
+                Spacer()
+                Text(String(Date().formatted(.dateTime.day().month().year())))
+            }
+            .padding()
+        }
+        .foregroundColor(showCard ? .white : .black)
+        .offset(y: showCard ? -280 : 10)
+        .overlay {
+            
+            Image("qrCode")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: showCard ? 300 : 250, height: showCard ? 300 : 250)
+                .offset(y: showCard ? screen.height - 800 : screen.height - 840)
+        }
     }
 }
