a703feda50bd7175ee1ba9e3a0c111c5ea6756da
Christopher <iacd07@IACD07s-Air.home>
2022-08-01 19:05:58 +0200

    home screen layout
    added news letter and adjusted ui

---
+++ IACD_EducationApp_StudentPortal/View/HomeView/Newsletter.swift
@@ -0,0 +1,22 @@
+//
+//  Newsletter.swift
+//  IACD_EducationApp_StudentPortal
+//
+//  Created by Christopher on 2022/08/01.
+//
+
+import SwiftUI
+
+struct Newsletter: View {
+    var body: some View {
+        Image("newsletter")
+            .resizable()
+            .ignoresSafeArea()
+    }
+}
+
+struct Newsletter_Previews: PreviewProvider {
+    static var previews: some View {
+        Newsletter()
+    }
+}
